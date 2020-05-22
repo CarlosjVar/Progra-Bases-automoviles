5a9e8e3021ca0c5dc2cd7bc3c69b7fe80589e607
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-05-21 21:02:26 -0600

    Template inicial para partes

--- src/main/java/com/negocio/automoviles/controllers/PartesController.java
+++ src/main/java/com/negocio/automoviles/controllers/PartesController.java
@@ -1,4 +1,16 @@
 package com.negocio.automoviles.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class PartesController {
+
+    @RequestMapping(value = "/partes", method = RequestMethod.GET)
+    public String partes(Model model) {
+        return "partes";
+    }
+
 }
