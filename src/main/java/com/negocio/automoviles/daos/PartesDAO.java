5699f3cedc7ecfd5a78f6c582b3e9644bca57848
Titovare <carlosjvare@gmail.com>
2020-05-21 21:25:36 -0600

    Inicio partes

--- src/main/java/com/negocio/automoviles/daos/PartesDAO.java
+++ src/main/java/com/negocio/automoviles/daos/PartesDAO.java
@@ -7,8 +7,5 @@ import javax.sql.DataSource;
 import java.util.List;
 
 public interface PartesDAO {
-    public void setDataSource(DataSource ds);
-    public Parte getParte(int ParteID);
-    public List<Parte> getPartes();
-    public void agregarParte(Parte parte);
+
 }
