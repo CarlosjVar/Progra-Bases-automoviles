af2d6b07c6608c0293abc83217af48ac3cf02e23
Titovare <carlosjvare@gmail.com>
2020-05-21 20:35:18 -0600

    Estado organizaciones listo

--- src/main/java/com/negocio/automoviles/controllers/PartesController.java
+++ src/main/java/com/negocio/automoviles/controllers/PartesController.java
@@ -1,4 +1,17 @@
 package com.negocio.automoviles.controllers;
-
+import com.negocio.automoviles.models.Parte;
+import com.negocio.automoviles.models.Persona;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class PartesController {
+    @RequestMapping(value = "/partes/add", method = RequestMethod.GET)
+    public String AddParte(Model model)
+    {
+        if (!model.containsAttribute("parte")) {
+        model.addAttribute("parte", new Parte());
+    }
+        return "addparte";
+    }
 }
--- src/main/java/com/negocio/automoviles/daos/PartesDAO.java
+++ src/main/java/com/negocio/automoviles/daos/PartesDAO.java
@@ -1,4 +1,14 @@
 package com.negocio.automoviles.daos;
 
+import com.negocio.automoviles.models.Organizacion;
+import com.negocio.automoviles.models.Parte;
+
+import javax.sql.DataSource;
+import java.util.List;
+
 public interface PartesDAO {
+    public void setDataSource(DataSource ds);
+    public Parte getParte(int ParteID);
+    public List<Parte> getPartes();
+    public void agregarParte(Parte parte);
 }
