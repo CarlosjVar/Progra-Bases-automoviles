9795fc57a98154b70e8a7f5b5a9a09e2ad935ba0
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-05-21 21:21:48 -0600

    Pagina principal de partes completa

--- src/main/java/com/negocio/automoviles/controllers/PartesController.java
+++ src/main/java/com/negocio/automoviles/controllers/PartesController.java
@@ -1,15 +1,34 @@
 package com.negocio.automoviles.controllers;
 
+import com.negocio.automoviles.database.DatabaseSource;
+import com.negocio.automoviles.jdbc.PartesJDBC;
+import com.negocio.automoviles.models.Parte;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
+/**
+ * Controlador para partes
+ *
+ */
 @Controller
 public class PartesController {
 
+    /**
+     * Carga la pagina principal para partes
+     * @param model El modelo para cargar datos
+     * @return La pagina principal de partes
+     */
     @RequestMapping(value = "/partes", method = RequestMethod.GET)
     public String partes(Model model) {
+        PartesJDBC partesJDBC = new PartesJDBC();
+        partesJDBC.setDataSource(DatabaseSource.getDataSource());
+        // Obtener las partes
+        List<Parte> partes = partesJDBC.getPartes();
+        model.addAttribute("partes", partes);
         return "partes";
     }
 
--- src/main/java/com/negocio/automoviles/daos/PartesDAO.java
+++ src/main/java/com/negocio/automoviles/daos/PartesDAO.java
@@ -1,4 +1,14 @@
 package com.negocio.automoviles.daos;
 
+import com.negocio.automoviles.models.Parte;
+import javax.sql.DataSource;
+import java.util.List;
+
+/**
+ * Partes Data Access Object
+ */
 public interface PartesDAO {
+    public void setDataSource(DataSource ds);
+    public List<Parte> getPartes();
+    public List<Parte> getPartesByModeloAnio(String modelo, int anio);
 }
