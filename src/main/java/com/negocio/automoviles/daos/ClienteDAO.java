04ca2d96d6c6d3a8f32f4dd24e0f2a53f5fc608b
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-05-27 18:57:42 -0600

    Obtener cliente activos y activar clientes

--- src/main/java/com/negocio/automoviles/daos/ClienteDAO.java
+++ src/main/java/com/negocio/automoviles/daos/ClienteDAO.java
@@ -2,14 +2,15 @@ package com.negocio.automoviles.daos;
 
 import javax.sql.DataSource;
 import java.util.List;
-
-import com.negocio.automoviles.models.Persona;
+import com.negocio.automoviles.models.Cliente;
 
 public interface ClienteDAO {
     public void setDataSource(DataSource ds);
     public int agregarCliente(String nombre, String direccion, String ciudad, String estado);
     public void modificarCliente(int id, String nombre, String direccion, String ciudad, String estado);
     public void suspenderCliente(int id);
+    public void activarCliente(int id);
+    public List<Cliente> getClientesActivos();
     public List<String> getEstados();
 }
 
--- src/main/java/com/negocio/automoviles/jdbc/ClientJDBC.java
+++ src/main/java/com/negocio/automoviles/jdbc/ClientJDBC.java
@@ -2,10 +2,16 @@ package com.negocio.automoviles.jdbc;
 
 import com.negocio.automoviles.daos.ClienteDAO;
 import com.negocio.automoviles.mappers.EstadoMapper;
+import com.negocio.automoviles.mappers.OrganizacionMapper;
+import com.negocio.automoviles.mappers.PersonaMapper;
+import com.negocio.automoviles.models.Cliente;
+import com.negocio.automoviles.models.Persona;
 import org.springframework.jdbc.core.JdbcTemplate;
+import com.negocio.automoviles.models.Organizacion;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -80,5 +86,43 @@ public class ClientJDBC implements ClienteDAO {
         String query = "SELECT estado FROM estados";
         List<String> estados = jdbcTemplateObject.query(query, new EstadoMapper());
         return estados;
+    }
+
+    /**
+     * Activar un cliente luego de realizar una orden
+     * @param id El id del cliente
+     */
+    @Override
+    public void activarCliente(int id) {
+        String query = "UPDATE clientes SET estado = 'ACTIVO' WHERE id = ?";
+        jdbcTemplateObject.update(query, id);
+    }
+
+    /**
+     * Obtiene todos los clientes activos
+     * @return La lista de clientes activos
+     */
+    @Override
+    public List<Cliente> getClientesActivos() {
+        // Obtener personas
+        String queryPersonas = "SELECT clientes.nombre, personas.cedula, clientes.estado, clientes.id, clientes.direccion, clientes.ciudad " +
+                "FROM clientes INNER JOIN personas ON clientes.id = personas.id_cliente WHERE NOT estado = 'SUSPENDIDO'";
+        List<Persona> personas = jdbcTemplateObject.query(queryPersonas, new PersonaMapper());
+        // Obtener organizaciones
+        String queryOrganizaciones = "SELECT clientes.nombre, organizaciones.cedula_juridica, clientes.estado, clientes.id, clientes.direccion, clientes.ciudad " +
+                ",organizaciones.encargado_nombre,organizaciones.encargado_telefono,organizaciones.encargado_cargo "
+                +
+                "FROM clientes INNER JOIN organizaciones ON clientes.id = organizaciones.surrogate_key";
+        List<Organizacion> organizacions = jdbcTemplateObject.query(queryOrganizaciones, new OrganizacionMapper());
+        List<Cliente> clientesActivos = new ArrayList<>();
+        // Agregar personas
+        for (Persona persona: personas) {
+            clientesActivos.add(persona);
+        }
+        // Agregar organizaciones
+        for (Organizacion organizacion : organizacions) {
+            clientesActivos.add(organizacion);
+        }
+        return clientesActivos;
     }
 }
