859d273a87ffde49196cbc2a43105d59bf173424
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-05-27 20:51:08 -0600

    Funcionalidad agregar orden

--- src/main/java/com/negocio/automoviles/controllers/OrdenesController.java
+++ src/main/java/com/negocio/automoviles/controllers/OrdenesController.java
@@ -2,12 +2,17 @@ package com.negocio.automoviles.controllers;
 
 import com.negocio.automoviles.database.DatabaseSource;
 import com.negocio.automoviles.jdbc.ClientJDBC;
+import com.negocio.automoviles.jdbc.OrdenJDBC;
+import com.negocio.automoviles.validators.OrdenValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
 
 /**
  * Controlador para ordenes
@@ -23,9 +28,33 @@ public class OrdenesController {
         return "addorden";
     }
 
+    /**
+     * Procesa una orden nueva
+     * @param cedula La cedula del cliente
+     * @param fecha La fecha de la orden
+     * @param redirectAttributes Los atributos para redirigir
+     * @return La pagina de ordenes
+     */
     @RequestMapping(value="/ordenes/add", method = RequestMethod.POST)
     public String procesarAddOrden(@RequestParam int cedula, @RequestParam String fecha, RedirectAttributes redirectAttributes) {
-
+        ArrayList<String> errors = new ArrayList<String>();
+        // Validar la cedula
+        int idCliente = OrdenValidator.validarClienteCedula(cedula);
+        if (idCliente == -1) {
+            errors.add("Ningun cliente posee esta cedula");
+        }
+        if (fecha.equals("")) {
+            errors.add("Por favor ingrese una fecha");
+        }
+        if (errors.size() > 0) {
+            redirectAttributes.addFlashAttribute("errors", errors);
+            return "redirect:/ordenes/add";
+        }
+        // Procesar la orden
+        OrdenJDBC ordenJDBC = new OrdenJDBC();
+        ordenJDBC.setDataSource(DatabaseSource.getDataSource());
+        ordenJDBC.crearOrdenNueva(idCliente, fecha);
+        redirectAttributes.addFlashAttribute("success_msg", "Orden creada");
         // TODO: Redirigir a la pagina principal de ordenes
         return "redirect:/";
     }
